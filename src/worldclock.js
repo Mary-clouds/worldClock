c9f7c179ab5dc61742229a950933e1da09a8f4e0
Mary <marynevermann@gmail.com>
2024-10-04 13:25:06 +0200

    added the html  element

---
+++ src/worldclock.js
@@ -0,0 +1,5 @@
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelDateElement = losAngelesElement.querySelector(".date");
+let losAngelTimeElement = losAngelesElement.querySelector(".time");
+losAngelDateElement.innerHTML= "Oktober 4 2022";
+losAngelTimeElement.innerHTML = " 7:56:13  <small>PM</small>";
