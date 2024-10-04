4498c9979e3be0405b0ad4fea2c847d63d2d667d
Mary <marynevermann@gmail.com>
2024-10-04 13:50:36 +0200

    added a updatetimezone function in js

--- src/worldclock.js
+++ src/worldclock.js
@@ -1,5 +1,20 @@
+function updateTimezone(){
+//los Angeles
 let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelDateElement = losAngelesElement.querySelector(".date");
 let losAngelTimeElement = losAngelesElement.querySelector(".time");
-losAngelDateElement.innerHTML= "Oktober 4 2022";
-losAngelTimeElement.innerHTML = " 7:56:13  <small>PM</small>";
+let timezonela = moment().tz("America/Los_Angeles");
+losAngelDateElement.innerHTML= timezonela.format("MMMM Do YYYY");
+losAngelTimeElement.innerHTML =  timezonela.format("h:mm:ss [<small>]A[</small>]");
+
+//douala
+let doualaElement = document.querySelector("#douala");
+let doualaDateElement = doualaElement.querySelector(".date");
+let doualaTimeElement = doualaElement.querySelector(".time");
+let timezonedouala = moment().tz("America/Los_Angeles");
+doualaDateElement.innerHTML= timezonedouala.format("MMMM Do YYYY");
+doualaTimeElement.innerHTML =  timezonedouala.format("h:mm:ss [<small>]A[</small>]");
+
+}
+updateTimezone();
+setInterval(updateTimezone,1000);
